0a390c50b6b9399db229a56319babe8b495045c4
Yejin Shin <shinyejin0212@gmail.com>
2023-07-08 20:20:30 +0900

    :rotating_light: Root 페이지 prettier 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,11 @@ export const metadata = META_ROOT;
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="ko" className={spoqaHanSansNeo.className}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
