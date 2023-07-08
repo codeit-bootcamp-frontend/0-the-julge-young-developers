3a6e01a71298feb13692afb423ae7c0c81b43c69
Yejin Shin <shinyejin0212@gmail.com>
2023-07-08 13:38:27 +0900

    🎉 프로젝트 시작
    - stylelint
    - scss
    - classnames & classnames type
    - axios

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: "더줄게",
+  description:
+    "급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
