27c067baf6a2a17047107662cdfe2d5c6d02cbcc
wooleejaan <wooleejaan@gmail.com>
2023-07-09 18:20:35 +0900

    :bug: Image import 방식 변경 및 Lint 적용

--- libs/footer/ui/ui-footer/footer.tsx
+++ libs/footer/ui/ui-footer/footer.tsx
@@ -1,13 +1,9 @@
+import classnames from 'classnames/bind'
+
 import Image from 'next/image'
 import Link from 'next/link'
 
-import email from '@/public/images/envelop-square.svg'
-import classnames from 'classnames/bind'
-
 import styles from '@/libs/footer/ui/ui-footer/footer.module.scss'
-
-import facebook from '../../../../../../../../public/images/facebook-square.svg'
-import instagram from '../../../../../../../../public/images/instagram.svg'
 
 const cx = classnames.bind(styles)
 
@@ -26,14 +22,24 @@ export default function Footer() {
         </div>
         <div className={cx('socials')}>
           <div className={cx('socialWrapper')}>
-            <Image src={email} alt="email" width={21.88} height={21.88} />
-          </div>
-          <div className={cx('socialWrapper')}>
-            <Image src={facebook} alt="facebook" width={21.88} height={21.88} />
+            <Image
+              src="/images/envelop-square.svg"
+              alt="email"
+              width={21.88}
+              height={21.88}
+            />
           </div>
           <div className={cx('socialWrapper')}>
             <Image
-              src={instagram}
+              src="/images/facebook-square.svg"
+              alt="facebook"
+              width={21.88}
+              height={21.88}
+            />
+          </div>
+          <div className={cx('socialWrapper')}>
+            <Image
+              src="/images/instagram.svg"
               alt="instagram"
               width={21.88}
               height={21.88}
