b421729e8958b3a7a6c50f3b5def25a2a9ddf726
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-11 14:50:19 +0900

    :lipstick: 버튼이 이미지를 받을 수 있도록 변경

--- libs/gnb/ui/ui-button/ui-button.tsx
+++ libs/gnb/ui/ui-button/ui-button.tsx
@@ -1,18 +1,32 @@
 import classNames from 'classnames/bind'
 
+import Image from 'next/image'
+
 import styles from './ui-button.module.scss'
 
 const cx = classNames.bind(styles)
 
 interface Props {
-  name: string
+  name?: string
+  activeStatus?: string
   handleClickButton: () => void
 }
 
-export default function UiButton({ name, handleClickButton }: Props) {
+export default function UiButton({
+  name,
+  activeStatus,
+  handleClickButton,
+}: Props) {
   return (
     <button type="button" onClick={handleClickButton} className={cx('button')}>
-      {name}
+      {name || (
+        <Image
+          src={`/images/notification-${activeStatus}.svg`}
+          alt="notification"
+          width={20}
+          height={20}
+        />
+      )}
     </button>
   )
 }
