b6e5d676371cc174bdbb6635fb7119ba6b37b408
wooleejaan <wooleejaan@gmail.com>
2023-07-11 17:38:43 +0900

    :sparkles: 공고 API 추상화 함수 타입 작성

--- libs/shared/api/type-api.d.ts
+++ libs/shared/api/type-api.d.ts
@@ -113,3 +113,163 @@ interface UserData {
   }
   links?: Links[]
 }
+
+/* noticeRequest */
+
+/* noticeRequest - getNotices */
+interface NoticesProps {
+  offset?: number
+  limit?: number
+  address?: string[]
+  keyword?: string
+  startsAtGte?: string
+  hourlyPayGte?: number
+  sort?: 'time' | 'pay' | 'hour' | 'shop'
+}
+
+interface NoticesItem {
+  item: {
+    id: string
+    hourlyPay: number
+    startsAt: string
+    workhour: number
+    description: string
+    closed: boolean
+    shop: {
+      item: {
+        id: string
+        name: string
+        category: string
+        address1: string
+        address2: string
+        description: string
+        imageUrl: string
+        originalHourlyPay: number
+      }
+      href: string
+    }
+  }
+  links?: Links[]
+}
+
+interface NoticesData {
+  offset: number
+  limit: number
+  address: string[]
+  keyword?: string
+  items: NoticesItem[]
+}
+
+/* noticeRequest - getShopNotices */
+
+interface ShopNoticesProps {
+  shopId: string
+  offset?: number
+  limit?: number
+}
+
+interface ShopNoticesItem {
+  item: {
+    id: string
+    hourlyPay: number
+    startsAt: string
+    workhour: number
+    description: string
+    closed: boolean
+  }
+  links?: Links[]
+}
+
+interface ShopNoticesData {
+  offset: number
+  limit: number
+  items: ShopNoticesItem[]
+  links?: Links[]
+}
+
+/* noticeRequest - registerShopNotice */
+
+interface RegisterdShopNoticeProps {
+  shopId: string
+  hourlyPay: number
+  startsAt: string
+  workhour: number
+  description: string
+}
+
+interface RegisterdShopNoticeData {
+  item: {
+    id: string
+    hourlyPay: number
+    startsAt: string
+    workhour: number
+    description: string
+    closed: boolean
+    shop: {
+      item: {
+        id: string
+        name: string
+        category: string
+        address1: string
+        address2: string
+        description: string
+        imageUrl: string
+        originalHourlyPay: number
+      }
+      href: string
+    }
+  }
+  links?: Links[]
+}
+
+/* noticeRequest - getShopNotice */
+
+interface ShopNoticeData {
+  item: {
+    id: string
+    hourlyPay: number
+    startsAt: string
+    workhour: number
+    description: string
+    closed: boolean
+    shop: {
+      item: {
+        id: string
+        name: string
+        category: string
+        address1: string
+        address2: string
+        description: string
+        imageUrl: string
+        originalHourlyPay: number
+      }
+      href: string
+    }
+  }
+  links?: Links[]
+}
+/* noticeRequest - updateShopNotice */
+interface UpdatedShopNotice {
+  item: {
+    id: string
+    hourlyPay: number
+    startsAt: string
+    workhour: number
+    description: string
+    closed: boolean
+    shop: {
+      item: {
+        id: string
+        name: string
+        category: string
+        address1: string
+        address2: string
+        description: string
+        imageUrl: string
+        originalHourlyPay: number
+      }
+      href: string
+    }
+  }
+  links?: Links[]
+}
