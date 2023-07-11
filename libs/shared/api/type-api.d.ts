6094a24c7347eede1dfe166524904b8dfbe3442b
wooleejaan <wooleejaan@gmail.com>
2023-07-11 14:29:04 +0900

    :sparkles: 유저/인증 요청 데이터 타입 지정

--- libs/shared/api/type-api.d.ts
+++ libs/shared/api/type-api.d.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* instance */
 import {
   AxiosInstance,
   AxiosInterceptorManager,
@@ -37,14 +39,77 @@ export interface CustomAxiosInterface extends AxiosInstance {
 }
 
 /* common */
-
-// data 안에 또 data가 있는 경우만 사용하는 타입입니다.
-// interface APIDataResponse<T> {
-//   data: T
-// }
-
 export interface CommonResponse<T> {
   data: T
   status: number
   statusText: string
+}
+
+/* userRequest */
+
+interface Links {
+  rel: string
+  description: string
+  method: string
+  href: string
+  query?: object[]
+  body?: object
+}
+
+/* userRequest - signIn */
+interface UserSignIn {
+  item: {
+    token: string
+    user: {
+      href: string
+      item: {
+        address: string
+        bio: string
+        email: string
+        id: string
+        name: string
+        phone: string
+        type: 'employee' | 'employer'
+      }
+    }
+  }
+  links?: []
+}
+
+/* userRequest - signUp */
+interface UserSignUp {
+  item: {
+    id: string
+    email: string
+    type: 'employee' | 'employer'
+  }
+  links?: Links[]
+}
+
+interface UserShop {
+  item: {
+    id: string
+    name: string
+    category: string
+    address1: string
+    address2: string
+    description: string
+    imageUrl: string
+    originalHourlyPay: number
+  }
+}
+
+/* userRequest - getUserInfo/updateUserInfo */
+interface UserData {
+  item: {
+    id: string
+    email: string
+    type: 'employer' | 'employee'
+    name?: string
+    phone?: string
+    address?: string
+    bio?: string
+    shop: UserShop | null
+  }
+  links?: Links[]
 }
