f0d36bc23812bae4fe721bf187758dd4b6526077
hayoung-99 <nhy990130@gmail.com>
2023-07-11 14:38:08 +0900

    :recycle: Badge -> CloseChip 컴포넌트 이름 변경

--- libs/shared/filter/ui/ui-close-chip/ui-close-chip.tsx
+++ libs/shared/filter/ui/ui-close-chip/ui-close-chip.tsx
@@ -2,17 +2,17 @@ import classNames from 'classnames/bind'
 
 import Image from 'next/image'
 
-import styles from './ui-badge.module.scss'
+import styles from './ui-close-chip.module.scss'
 
-interface UiBadgeProps {
+interface UiCloseChipProps {
   children: string
 }
 
 const cx = classNames.bind(styles)
 
-export default function UiBadge({ children }: UiBadgeProps) {
+export default function UiCloseChip({ children }: UiCloseChipProps) {
   return (
-    <button className={cx('badge')} type="button">
+    <button className={cx('closeChip')} type="button">
       {children}
       <Image src="/images/close-red.svg" alt="해제" width={16} height={16} />
     </button>
