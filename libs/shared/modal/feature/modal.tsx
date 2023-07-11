e98bec820accdb7bae6614df09ece1ae434fee69
hayoung-99 <nhy990130@gmail.com>
2023-07-11 17:58:11 +0900

    :bulb: Modal 컴포넌트 jsdoc 추가

--- libs/shared/modal/feature/modal.tsx
+++ libs/shared/modal/feature/modal.tsx
@@ -2,10 +2,22 @@ import { acceptTextOptions } from '@/libs/shared/modal/data-access/data-access-m
 import UiActionModal from '@/libs/shared/modal/ui/ui-action-modal/ui-action-modal'
 import UiConfirmModal from '@/libs/shared/modal/ui/ui-confirm-modal/ui-confirm-modal'
 
+/**
+ * @param text 모달 본문 내용
+ * @param onConfirm '확인' 버튼 이벤트 핸들러
+ * @returns Confirm 모달 UI 리턴
+ */
 function ConfirmModal({ text, onConfirm }: ConfirmModalProps) {
   return <UiConfirmModal text={text} confirmText="확인" onConfirm={onConfirm} />
 }
 
+/**
+ * @param type Action 모달 type - 'reject' | 'accept' | 'cancel'
+ * @param text 모달 본문 내용
+ * @param onCancel cancel 버튼 이벤트 핸들러
+ * @param onAccept accept 버튼 이벤트 핸들러
+ * @returns Action 모달 UI 리턴
+ */
 function ActionModal({ type, text, onCancel, onAccept }: ActionModalProps) {
   return (
     <UiActionModal
