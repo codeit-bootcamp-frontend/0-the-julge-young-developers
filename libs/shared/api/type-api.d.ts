44f1a78e6b369cc7968c58e5bb0f77b4bc23edff
wooleejaan <wooleejaan@gmail.com>
2023-07-11 20:38:50 +0900

    :sparkles: Alerts 타입 정의

--- libs/shared/api/type-api.d.ts
+++ libs/shared/api/type-api.d.ts
@@ -372,3 +372,54 @@ interface NoticeUserApplicationData {
   items: NoticeUserApplicationItem[]
   links?: Links[]
 }
+
+/* alertsRequest */
+/* alertsRequest - getUserAlertsList */
+
+interface UserAlertsListItem {
+  item: {
+    id: string
+    createdAt: string
+    result: 'accepted' | 'rejected'
+    read: boolean
+    application: {
+      item: {
+        id: string
+        status: 'pending' | 'accepted' | 'rejected'
+      }
+      href: string
+    }
+    shop: {
+      item: {
+        id: string
+        name: string
+        category: string
+        address1: string
+        address2: string
+        description: string
+        imageUrl: string
+        originalHourlyPay: number
+      }
+      href: string
+    }
+    notice: {
+      item: {
+        id: string
+        hourlyPay: number
+        description: string
+        startsAt: string
+        workhour: number
+        closed: boolean
+      }
+      href: string
+    }
+    links?: Links[]
+  }
+}
+interface UserAlertsListData {
+  offset: number
+  limit: number
+  items: UserAlertsListItem[]
+}
+
+/* alertsRequest - clearAlerts */
