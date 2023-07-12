16b6b22e0810f5089263ed33c19f3d9942d2843c
drizzle96 <dlxkffldk37@naver.com>
2023-07-12 16:36:59 +0900

    :sparkles: 로그인 기본 UI 구현

---
+++ libs/signin/feature/signin.tsx
@@ -0,0 +1,6 @@
+import UiSignIn from '@/libs/signin/ui/ui-signin/ui-signin'
+
+export default function SignIn() {
+  const handleSignIn = () => null
+  return <UiSignIn onSignIn={handleSignIn} />
+}
---
+++ libs/signin/ui/ui-signin/ui-signin.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import classnames from 'classnames/bind'
+
+import { CommonActiveBtn } from '@/libs/shared/common-click-btn/feature/common-btn'
+import UiCommonLayout from '@/libs/shared/common-layout/ui/ui-common-layout/ui-common-layout'
+import Input from '@/libs/shared/input-select-btn/feature/feature-input'
+
+import styles from './ui-signin.module.scss'
+
+const cx = classnames.bind(styles)
+
+export default function UiSignIn({ onSignIn }: { onSignIn: () => null }) {
+  const handleClickButton = () => {
+    onSignIn()
+  }
+
+  return (
+    <div className={cx('wrapper')}>
+      <UiCommonLayout
+        title="로그인"
+        titleAlign="center"
+        titleSize={28}
+        gap={40}
+      >
+        <form className={cx('form')}>
+          <Input
+            variant="input"
+            title="이메일"
+            isValid={false}
+            isRequired={true}
+          />
+          <Input
+            variant="input"
+            title="비밀번호"
+            isValid={false}
+            isRequired={true}
+          />
+          <CommonActiveBtn
+            text="로그인 하기"
+            size="large"
+            onClick={handleClickButton}
+          />
+        </form>
+      </UiCommonLayout>
+    </div>
+  )
+}
