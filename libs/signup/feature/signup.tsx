515a5b2c49e8d5a47a3387cc42d98a7b33558ad2
drizzle96 <dlxkffldk37@naver.com>
2023-07-12 19:22:18 +0900

    :sparkles: 회원가입 기본 UI 구현

---
+++ libs/signup/feature/signup.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import UiSignUp from '@/libs/signup/ui/ui-signup/ui-signup'
+
+export default function SignUp() {
+  const handleClickSignUp = () => null
+  return <UiSignUp onClickSignUp={handleClickSignUp} />
+}
