300926149abd060f7d76d5843767f1e22153d073
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-12 16:10:10 +0900

    :lipstick: Handler 함수 파라미터 타입 변경

--- libs/signup/ui/ui-button-container/ui-button-container.tsx
+++ libs/signup/ui/ui-button-container/ui-button-container.tsx
@@ -7,13 +7,14 @@ import classNames from 'classnames/bind'
 import UiTypeButton from '@/libs/signup/ui/ui-type-button/ui-type-button'
 
 import styles from './ui-button-container.module.scss'
+import { UserType } from '../../type-signup'
 
 const cx = classNames.bind(styles)
 
 export default function UiButtonContainer() {
   const [userType, setUserType] = useState('employee')
 
-  const handleClickSelectUserType = (type: string) => {
+  const handleClickSelectUserType = (type: UserType) => {
     if (userType !== type) {
       setUserType(type)
     }
