43c9ebd5d560e19be72a59223c46076448f2e0d0
Yejin Shin <shinyejin0212@gmail.com>
2023-07-13 14:34:25 +0900

    :fire: Home page 초기화
    테스트를 위해 사용했던 컴포넌트 코드 제거

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,3 @@
-import { options } from '@/libs/shared/input-select-btn/data-access/mock-data'
-import Select from '@/libs/shared/input-select-btn/feature/feature-select'
-
 export default function Home() {
-  return (
-    <main>
-      <Select
-        variant="filter"
-        // variant="search"
-        // title="하이루"
-        // isRequired={false}
-        options={options}
-        defaultValueIdx={0}
-      />
-    </main>
-  )
+  return <main>Home</main>
 }
