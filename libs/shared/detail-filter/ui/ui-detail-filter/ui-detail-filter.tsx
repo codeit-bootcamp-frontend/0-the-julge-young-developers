fd53af12bd654f7ae2b7953a4737e04e68db824f
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-14 14:45:43 +0900

    :bug: Detail-Filter 레이아웃 수정

--- libs/shared/detail-filter/ui/ui-detail-filter/ui-detail-filter.tsx
+++ libs/shared/detail-filter/ui/ui-detail-filter/ui-detail-filter.tsx
@@ -14,7 +14,6 @@ import UiDivider from '@/libs/shared/detail-filter/ui/ui-divider/ui-divider'
 import UiLocationContainer from '@/libs/shared/detail-filter/ui/ui-location-container/ui-location-container'
 import UiSelectedChip from '@/libs/shared/detail-filter/ui/ui-selected-chip/ui-selected-chip'
 import Input from '@/libs/shared/input-select-btn/feature/feature-input'
-import UiSimpleLayout from '@/libs/shared/simple-layout/ui/ui-simple-layout/ui-simple-layout'
 
 import styles from './ui-detail-filter.module.scss'
 
@@ -32,63 +31,72 @@ export default function UiDetailFilter({
 }: UiDetailFilterProps) {
   return (
     <div className={cx('filterContainer')}>
-      <button
-        className={cx('closeButton')}
-        type="button"
-        onClick={onClickCloseButton}
-      >
-        <Image src="/images/close.svg" alt="닫기" width={24} height={24} />
-      </button>
-      <UiSimpleLayout title="상세 필터" titleSize={20} gap={24}>
-        <div className={cx('filterContent')}>
-          <div className={cx('section')}>
-            <h2 className={cx('subtitle')}>위치</h2>
-            <UiLocationContainer
-              locations={LOCATIONS}
-              onSelect={onSelectLocation}
-            />
-            {selectedLocations.size !== 0 && (
-              <div className={cx('selectedChipContainer')}>
-                {Array.from(selectedLocations).map((location) => (
-                  <UiSelectedChip key={location} onCancel={onCancelLocation}>
-                    {location}
-                  </UiSelectedChip>
-                ))}
-              </div>
-            )}
-          </div>
-          <UiDivider />
-          <div className={cx('section')}>
+      <div className={cx('filterHeader')}>
+        <h1>상세 필터</h1>
+        <button
+          className={cx('closeButton')}
+          type="button"
+          onClick={onClickCloseButton}
+        >
+          <Image src="/images/close.svg" alt="닫기" width={24} height={24} />
+        </button>
+      </div>
+      <div className={cx('filterContent')}>
+        <div className={cx('section')}>
+          <h2 className={cx('subtitle')}>위치</h2>
+          <UiLocationContainer
+            locations={LOCATIONS}
+            onSelect={onSelectLocation}
+          />
+          {selectedLocations.size !== 0 && (
+            <div className={cx('selectedChipContainer')}>
+              {Array.from(selectedLocations).map((location) => (
+                <UiSelectedChip key={location} onCancel={onCancelLocation}>
+                  {location}
+                </UiSelectedChip>
+              ))}
+            </div>
+          )}
+        </div>
+        <UiDivider />
+        <div className={cx('section')}>
+          <Input
+            variant="input"
+            title="시작일"
+            isRequired={true}
+            ref={startInputRef}
+          />
+        </div>
+        <UiDivider />
+        <div className={cx('section')}>
+          <div className={cx('priceInput')}>
             <Input
               variant="input"
-              title="시작일"
+              title="금액"
               isRequired={true}
-              ref={startInputRef}
+              suffix="원"
+              ref={priceInputRef}
             />
-          </div>
-          <UiDivider />
-          <div className={cx('section')}>
-            <div className={cx('priceInput')}>
-              <Input
-                variant="input"
-                title="금액"
-                isRequired={true}
-                suffix="원"
-                ref={priceInputRef}
-              />
-              <span className={cx('sideText')}>이상부터</span>
-            </div>
+            <span className={cx('sideText')}>이상부터</span>
           </div>
         </div>
-        <div className={cx('buttonContainer')}>
-          <div className={cx('initButton')}>
-            <ActiveOutlineBtn text="초기화" size="large" onClick={onClickInitButton} />
-          </div>
-          <div className={cx('applyButton')}>
-            <ActiveBtn text="적용하기" size="large" onClick={onClickApplyButton} />
-          </div>
+      </div>
+      <div className={cx('buttonContainer')}>
+        <div className={cx('initButton')}>
+          <ActiveOutlineBtn
+            text="초기화"
+            size="large"
+            onClick={onClickInitButton}
+          />
         </div>
-      </UiSimpleLayout>
+        <div className={cx('applyButton')}>
+          <ActiveBtn
+            text="적용하기"
+            size="large"
+            onClick={onClickApplyButton}
+          />
+        </div>
+      </div>
     </div>
   )
 }
