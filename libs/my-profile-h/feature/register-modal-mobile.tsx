6a511c6bf02fc65721f8738794b07b3e0cfdfc9f
wooleejaan <wooleejaan@gmail.com>
2023-07-14 14:52:22 +0900

    :sparkles: 반응형 Input Modal 컴포넌트 제작

---
+++ libs/my-profile-h/feature/register-modal-mobile.tsx
@@ -0,0 +1,9 @@
+import ModalPortalWrapper from '@/libs/portal/feature/modalWrapper'
+
+export default function RegisterModalMobile() {
+  return (
+    <ModalPortalWrapper id="funnel-portal">
+      <div>모바일입니다.</div>
+    </ModalPortalWrapper>
+  )
+}
---
+++ libs/my-profile-h/feature/register-modal.tsx
@@ -0,0 +1,9 @@
+import ModalPortalWrapper from '@/libs/portal/feature/modalWrapper'
+
+export default function RegisterModal() {
+  return (
+    <ModalPortalWrapper id="funnel-portal">
+      <div>데스크탑/태블릿입니다.</div>
+    </ModalPortalWrapper>
+  )
+}
