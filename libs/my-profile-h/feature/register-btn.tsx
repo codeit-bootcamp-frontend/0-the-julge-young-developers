5c9f8b667d4b31b27ba56b1b9decc3240ddced21
wooleejaan <wooleejaan@gmail.com>
2023-07-15 11:18:56 +0900

    :sparkles: 등록하기 버튼에 Input Modal 적용
    모바일 버전, 데스크탑/태블릿 버전 분기 처리

--- libs/my-profile-h/feature/register-btn.tsx
+++ libs/my-profile-h/feature/register-btn.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 
-import UiCommonClickBtn from '@/libs/shared/click-btns/ui/ui-click-btn'
+import { ActiveBtn } from '@/libs/shared/click-btns/feature/click-btns'
 import { useMediaQuery } from '@/libs/shared/shared/util/useMediaQuery'
 
 import RegisterModal from './register-modal'
@@ -10,30 +10,40 @@ import RegisterModalMobile from './register-modal-mobile'
 
 export default function RegisterBtn() {
   const isMobile = useMediaQuery('(max-width: 768px)')
-  const isTablet = useMediaQuery('(max-width: 1200px)')
+  const isTablet = useMediaQuery('(max-width: 1028px)')
   const [openView, setOpenView] = useState<
     'not-found' | 'tablet/desktop' | 'mobile'
   >('not-found')
+  const [openModal, setOpenModal] = useState(false)
+
+  const handleClickCloseModal = () => {
+    setOpenModal(false)
+  }
 
   const handleClickChangeView = () => {
     if (isMobile && isTablet) {
+      setOpenModal(true)
       setOpenView('mobile')
     } else {
+      setOpenModal(true)
       setOpenView('tablet/desktop')
     }
   }
 
   return (
     <div>
-      <UiCommonClickBtn
+      <ActiveBtn
         text="내 프로필 등록하기"
-        size="large"
-        status="active"
+        size={isMobile ? 'mediumSmall' : 'large'}
         onClick={handleClickChangeView}
       />
 
-      {openView === 'tablet/desktop' && <RegisterModal />}
-      {openView === 'mobile' && <RegisterModalMobile />}
+      {openModal && !isMobile && openView === 'tablet/desktop' && (
+        <RegisterModal onClickCloseModal={handleClickCloseModal} />
+      )}
+      {openModal && isMobile && openView === 'mobile' && (
+        <RegisterModalMobile onClickCloseModal={handleClickCloseModal} />
+      )}
     </div>
   )
 }
