cceb138829467c878e20a15e621b0bc98bfc4189
drizzle96 <dlxkffldk37@naver.com>
2023-07-15 09:35:41 +0900

    :sparkles: MOCK_SHOP, Shop 타입 설정

---
+++ libs/my-shop/type-my-shop.ts
@@ -0,0 +1,22 @@
+const SHOP_CATEGORY = {
+  Korean: '한식',
+  Chinese: '중식',
+  Japanese: '일식',
+  Western: '양식',
+  SnackBar: '분식',
+  Cafe: '카페',
+  Convenience: '편의점',
+  Other: '기타',
+} as const
+export type ShopCategory = (typeof SHOP_CATEGORY)[keyof typeof SHOP_CATEGORY]
+
+export interface Shop {
+  id: string
+  name: string
+  category: ShopCategory
+  address1: string
+  address2: string
+  description: string
+  imageUrl: string
+  originalHourlyPay: number
+}
