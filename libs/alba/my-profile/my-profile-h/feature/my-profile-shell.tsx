c769844f1321c8a05d9fa791c6c2e0ad9d1aca5d
wooleejaan <wooleejaan@gmail.com>
2023-07-17 14:03:55 +0900

    :sparkles: Alba 내 프로필 도메인 레이아웃 스타일 추가

--- libs/alba/my-profile/my-profile-h/feature/my-profile-shell.tsx
+++ libs/alba/my-profile/my-profile-h/feature/my-profile-shell.tsx
@@ -1,8 +1,13 @@
+import classNames from 'classnames/bind'
+
 import MyProfile from '@/libs/alba/my-profile/my-profile-alice2/feature/my-profile'
 import UiRegisterLayout from '@/libs/shared/register-layout/ui/ui-register-layout'
 import UiSimpleLayout from '@/libs/shared/simple-layout/ui/ui-simple-layout/ui-simple-layout'
 
+import styles from './my-profile-shell.module.scss'
 import RegisterBtn from './register-btn'
+
+const cx = classNames.bind(styles)
 
 interface MyProfileShellProps {
   isRegistered: boolean
@@ -19,14 +24,16 @@ export default function MyProfileShell({
   userProfile,
 }: MyProfileShellProps) {
   return (
-    <UiSimpleLayout title="내 프로필" gap={24}>
-      {!isRegistered && (
-        <UiRegisterLayout
-          text="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
-          registerButton={<RegisterBtn />}
-        />
-      )}
-      {isRegistered && userProfile && <MyProfile {...userProfile} />}
-    </UiSimpleLayout>
+    <div className={cx('profileWrapper')}>
+      <UiSimpleLayout title="내 프로필" gap={24}>
+        {!isRegistered && (
+          <UiRegisterLayout
+            text="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
+            registerButton={<RegisterBtn />}
+          />
+        )}
+        {isRegistered && userProfile && <MyProfile {...userProfile} />}
+      </UiSimpleLayout>
+    </div>
   )
 }
