6334c45f4dac7e3039e36f3c9033a68d61ccb2cc
hayoung-99 <nhy990130@gmail.com>
2023-07-18 00:46:40 +0900

    :sparkles: 지원 가능 여부에 따라 공고 지원 버튼 다르게 렌더링
    1. 공고가 closed: 지원 불가
    2. 유저가 해당 공고에 지원한 경우: 취소하기
    3. 유저가 해당 공고에 지원하지 않은 경우: 신청하기

---
+++ libs/notice-detail-alice/ui/ui-application-button.tsx
@@ -0,0 +1,43 @@
+import {
+  ActiveBtn,
+  ActiveOutlineBtn,
+  InactiveBtn,
+} from '@/libs/shared/click-btns/feature/click-btns'
+
+export default function ApplicationButton({
+  type,
+}: {
+  type: 'application' | 'cancel' | 'disabled'
+}) {
+  if (type === 'application') {
+    return (
+      <ActiveBtn
+        text="신청하기"
+        size="large"
+        onClick={() => {
+          console.log('신청하기~!')
+        }}
+      />
+    )
+  }
+
+  if (type === 'cancel') {
+    return (
+      <ActiveOutlineBtn
+        text="취소하기"
+        size="large"
+        onClick={() => console.log('취소하기')}
+      />
+    )
+  }
+
+  if (type === 'disabled') {
+    return (
+      <InactiveBtn
+        text="신청 불가"
+        size="large"
+        onClick={() => console.log('신청 불가')}
+      />
+    )
+  }
+}
