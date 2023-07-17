af91f1e148851eccc955f9a5bd0aee825c1654ad
drizzle96 <dlxkffldk37@naver.com>
2023-07-17 14:45:30 +0900

    :recycle: 공고 상세 데이터 api 함수 타입 및 내용 수정

--- libs/my-shop-notice/data-access/data-access-notice.ts
+++ libs/my-shop-notice/data-access/data-access-notice.ts
@@ -1,9 +1,12 @@
 import { getNoticeApplicationList } from '@/libs/shared/api/data-access/request/applicationsRequest'
 import { getShopNotice } from '@/libs/shared/api/data-access/request/noticeRequest'
 import { getUserInfo } from '@/libs/shared/api/data-access/request/userRequest'
+import { NoticeDetail } from '@/libs/shared/notice-card/type-notice-card'
 import { utilFormatDuration } from '@/libs/shared/shared/util/utilFormatDuration'
 import { MOCK_APPLICANT_LIST_DATA } from '@/libs/shared/table/data-access/data-access-mock'
 import { ApplicantList } from '@/libs/shared/table/type-table'
+
+import { MOCK_NOTICE_DETAIL_DATA } from './data-access-mock'
 
 export const getShopIdData = async (userId: string) => {
   const res = await getUserInfo(userId)
@@ -17,50 +20,56 @@ export const getShopIdData = async (userId: string) => {
   }
 }
 
-export const getNoticeDetailData = async (shopId: string, noticeId: string) => {
+export const getNoticeDetailData = async (
+  shopId: string,
+  noticeId: string,
+): Promise<NoticeDetail> => {
   const res = await getShopNotice(shopId, noticeId)
   if (res instanceof Error) {
     // 알 수 없는 에러 처리
-  } else if (typeof res === 'string') {
-    // 에러 메시지에 맞게 처리
-  } else {
-    // response 데이터 가공
-    const notice = res.item
-    const {
-      id,
-      hourlyPay,
-      startsAt,
-      workhour,
-      description: noticeDescription,
-      closed,
-      shop,
-    } = notice
-
-    const {
-      name,
-      category,
-      address1: address,
-      description: shopDescription,
-      imageUrl,
-      originalHourlyPay,
-    } = shop.item
-
-    const noticeDetail = {
-      id,
-      name,
-      category,
-      duration: utilFormatDuration(startsAt, workhour),
-      address,
-      shopDescription,
-      noticeDescription,
-      hourlyPay,
-      originalHourlyPay,
-      imageUrl,
-      closed,
-    }
-
-    return noticeDetail
+    return MOCK_NOTICE_DETAIL_DATA
   }
+  if (typeof res === 'string') {
+    // 에러 메시지에 맞게 처리
+    return MOCK_NOTICE_DETAIL_DATA
+  }
+  // response 데이터 가공
+  const notice = res.item
+  const {
+    id,
+    hourlyPay,
+    startsAt,
+    workhour,
+    description: noticeDescription,
+    closed,
+    shop,
+  } = notice
+
+  const {
+    name,
+    category,
+    address1: address,
+    description: shopDescription,
+    imageUrl,
+    originalHourlyPay,
+  } = shop.item
+
+  const noticeDetail = {
+    id,
+    name,
+    category,
+    duration: utilFormatDuration(startsAt, workhour),
+    workhour,
+    address,
+    shopDescription,
+    noticeDescription,
+    hourlyPay,
+    originalHourlyPay,
+    imageUrl,
+    closed,
+  }
+
+  return noticeDetail
 }
 
 const LIST_PER_PAGE = 5
