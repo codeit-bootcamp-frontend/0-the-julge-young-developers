a6f7a0a968c4d51db72b998a86e6c1f3e782c212
wooleejaan <wooleejaan@gmail.com>
2023-07-17 15:53:18 +0900

    :bug: 내 프로필 도메인 클라이언트 컴포넌트로 변경

--- libs/alba/my-profile/my-profile-h/feature/my-profile-shell.tsx
+++ libs/alba/my-profile/my-profile-h/feature/my-profile-shell.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import classNames from 'classnames/bind'
 
 import MyProfile from '@/libs/alba/my-profile/my-profile-alice2/feature/my-profile'
 import UiRegisterLayout from '@/libs/shared/register-layout/ui/ui-register-layout'
+import { useMediaQuery } from '@/libs/shared/shared/util/useMediaQuery'
 import UiSimpleLayout from '@/libs/shared/simple-layout/ui/ui-simple-layout/ui-simple-layout'
 
 import styles from './my-profile-shell.module.scss'
@@ -23,9 +26,14 @@ export default function MyProfileShell({
   isRegistered,
   userProfile,
 }: MyProfileShellProps) {
+  const isMobile = useMediaQuery('(max-width: 768px)')
   return (
     <div className={cx('profileWrapper')}>
-      <UiSimpleLayout title="내 프로필" gap={24}>
+      <UiSimpleLayout
+        titleSize={isMobile ? 20 : 28}
+        title="내 프로필"
+        gap={isMobile ? 16 : 32}
+      >
         {!isRegistered && (
           <UiRegisterLayout
             text="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
