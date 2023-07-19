3fdfcb98fd7c76e3bfa26050c0c5091e61055caa
Yejin Shin <shinyejin0212@gmail.com>
2023-07-20 01:55:18 +0900

    :sparkles: 공고 수정하기 에러 처리

--- libs/my-shop-notice/feature/my-shop-notice-edit-button.tsx
+++ libs/my-shop-notice/feature/my-shop-notice-edit-button.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* eslint-disable no-unused-vars */
 import { useState } from 'react'
+
+import { useRouter } from 'next/navigation'
 
 import RegisterNoticeModal from '@/libs/my-shop/feature/my-notice/register-notice-modal'
 import { ActiveOutlineBtn } from '@/libs/shared/click-btns/feature/click-btns'
+import { ConfirmDialog } from '@/libs/shared/dialog/feature/dialog'
 import ToastContainer from '@/libs/shared/toast/feature/toast-container'
 
 import { MyShopNoticeEditButtonProps } from '../type-my-shop-notice'
@@ -17,6 +17,31 @@ export default function MyShopNoticeEditButton({
   const [shownEditModal, setShownEditModal] = useState(false)
   const [showModal, setShowModal] = useState<boolean>(false)
   const [showToast, setShowToast] = useState<boolean>(false)
+  const [openErrorDialog, setOpenErrorDialog] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>('')
+  const router = useRouter()
+
+  const handleClickShowErrorDialog = (text: string) => {
+    setErrorMessage(text)
+    setOpenErrorDialog(true)
+    switch (text) {
+      case '로그인이 필요합니다':
+        router.push('/signin')
+        break
+      case '공고를 수정할 권한이 없습니다':
+        router.push('/')
+        break
+      case '존재하지 않는 가게입니다' || '존재하지 않는 공고입니다':
+        router.push('/my-shop')
+        break
+      case '마감된 공고는 수정할 수 없습니다':
+        setShownEditModal(false)
+        break
+      default:
+        break
+    }
+  }
+
   const handleClickButton = () => {
     setShownEditModal((prev) => !prev)
 
@@ -43,12 +68,19 @@ export default function MyShopNoticeEditButton({
           showModal={showModal}
           notice={notice}
           onClickShowToast={() => setShowToast(true)}
+          onClickShowErrorDialog={handleClickShowErrorDialog}
         />
       )}
       {showToast && (
         <ToastContainer onShow={() => setShowToast(false)}>
           편집을 완료했어요
         </ToastContainer>
+      )}
+      {openErrorDialog && (
+        <ConfirmDialog
+          text={errorMessage || '요청에 실패했습니다.'}
+          onConfirm={() => setOpenErrorDialog(false)}
+        />
       )}
     </>
   )
