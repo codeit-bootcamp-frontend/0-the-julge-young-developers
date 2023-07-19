7a1135655ea525dd590c3bab6e1cb10f2641dc35
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-19 20:01:30 +0900

    :lipstick: 전체 gnb 감싸는 div 추가

--- libs/gnb/ui/ui-gnb/ui-gnb.tsx
+++ libs/gnb/ui/ui-gnb/ui-gnb.tsx
@@ -18,57 +18,62 @@ export default function UiGnb({
   onCheckNotification,
 }: GnbProps) {
   return (
-    <nav className={cx('gnb')}>
-      <div className={cx('wrapper')}>
-        <Link href="/" className={cx('logo')}>
-          <Image
-            src="/images/logo.svg"
-            alt="logo"
-            width={112}
-            height={40}
-            priority={true}
-            className={cx('logo')}
-          />
-        </Link>
-        <div className={cx('searchbar')}>{searchbarElement}</div>
+    <div>
+      <nav className={cx('gnb')}>
+        <div className={cx('wrapper')}>
+          <Link href="/" className={cx('logo')}>
+            <Image
+              src="/images/logo.svg"
+              alt="logo"
+              width={112}
+              height={40}
+              priority={true}
+              className={cx('logo')}
+            />
+          </Link>
+          <div className={cx('searchbar')}>{searchbarElement}</div>
 
-        {userType === 'guest' && (
-          <div className={cx('buttons')}>
-            <UiButton
-              name="로그인"
-              id="login"
-              handleClickButton={() => handleClickMovePage('signin')}
-            />
-            <UiButton
-              name="회원가입"
-              id="signup"
-              handleClickButton={() => handleClickMovePage('signup')}
-            />
-          </div>
-        )}
-        {userType === 'employee' && (
-          <div className={cx('buttons')}>
-            <UiButton
-              name="내 프로필"
-              handleClickButton={() => handleClickMovePage('my-profile')}
-            />
-            <NotiButton
-              onCheckNotification={onCheckNotification}
-              activeStatus={hasNotification ? 'active' : 'inactive'}
-            />
-          </div>
-        )}
+          {userType === 'guest' && (
+            <div className={cx('buttons')}>
+              <UiButton
+                name="로그인"
+                id="login"
+                handleClickButton={() => handleClickMovePage('signin')}
+              />
+              <UiButton
+                name="회원가입"
+                id="signup"
+                handleClickButton={() => handleClickMovePage('signup')}
+              />
+            </div>
+          )}
+          {userType === 'employee' && (
+            <div className={cx('buttons')}>
+              <UiButton
+                name="내 프로필"
+                handleClickButton={() => handleClickMovePage('my-profile')}
+              />
+              <NotiButton
+                onCheckNotification={onCheckNotification}
+                activeStatus={hasNotification ? 'active' : 'inactive'}
+              />
+            </div>
+          )}
 
-        {userType === 'employer' && (
-          <div className={cx('buttons')}>
-            <UiButton name="내 가게" handleClickButton={handleClickMovePage} />
-            <NotiButton
-              onCheckNotification={onCheckNotification}
-              activeStatus={hasNotification ? 'active' : 'inactive'}
-            />
-          </div>
-        )}
-      </div>
-    </nav>
+          {userType === 'employer' && (
+            <div className={cx('buttons')}>
+              <UiButton
+                name="내 가게"
+                handleClickButton={handleClickMovePage}
+              />
+              <NotiButton
+                onCheckNotification={onCheckNotification}
+                activeStatus={hasNotification ? 'active' : 'inactive'}
+              />
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
   )
 }
