2672418b326249c341078a2ccb579fb38f92cd05
drizzle96 <dlxkffldk37@naver.com>
2023-07-19 22:23:51 +0900

    :sparkles: 가게 등록 토스트 UI 구현

--- libs/my-shop/feature/my-shop/register-shop-btn.tsx
+++ libs/my-shop/feature/my-shop/register-shop-btn.tsx
@@ -1,15 +1,29 @@
 'use client'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 
 import { ActiveBtn } from '@/libs/shared/click-btns/feature/click-btns'
+import { ConfirmDialog } from '@/libs/shared/dialog/feature/dialog'
 import { useMediaQuery } from '@/libs/shared/shared/util/useMediaQuery'
+import ToastContainer from '@/libs/shared/toast/feature/toast-container'
 
 import RegisterShopModal from './register-shop-modal'
 
 export default function RegisterShopBtn() {
   const [openModal, setOpenModal] = useState<boolean>(false)
   const isMobile = useMediaQuery('(max-width: 768px)')
+  const [openErrorDialog, setOpenErrorDialog] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>('')
+  const [showToast, setShowToast] = useState(false)
+
+  const handleClickShowErrorDialog = (text: string) => {
+    setErrorMessage(text)
+    setOpenErrorDialog(true)
+  }
+
   const handleClickToggleModal = () => {
     setOpenModal(!openModal)
   }
@@ -23,7 +37,22 @@ export default function RegisterShopBtn() {
       />
 
       {openModal && (
-        <RegisterShopModal onClickToggelModal={handleClickToggleModal} />
+        <RegisterShopModal
+          onClickToggelModal={handleClickToggleModal}
+          onClickShowToast={() => setShowToast(true)}
+          // handleClickShowErrorDialog={handleClickShowErrorDialog}
+        />
+      )}
+      {/* {openErrorDialog && (
+        <ConfirmDialog
+          text={errorMessage || '요청에 실패했습니다.'}
+          onConfirm={() => setOpenErrorDialog(false)}
+        />
+      )} */}
+      {showToast && (
+        <ToastContainer onShow={() => setShowToast(false)}>
+          등록을 완료했어요
+        </ToastContainer>
       )}
     </div>
   )
