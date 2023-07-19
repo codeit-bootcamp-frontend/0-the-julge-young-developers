7bf3d17dc03b0e6333951a0c8bfbdb1e85d715d3
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-19 22:54:35 +0900

    :sparkles: Page에서 내려준 prop 사용

--- libs/search-notice-list/feature/search-notice-list.tsx
+++ libs/search-notice-list/feature/search-notice-list.tsx
@@ -1,16 +1,32 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-
+// import { useSearchParams } from 'next/navigation'
 import NoticeList from '@/libs/notice-list/feature/notice-list'
+import { AllNoticesData } from '@/libs/shared/api/types/type-notice'
 
-export default function SearchNoticeList() {
-  const searchParams = useSearchParams()
-  const keyword = searchParams.get('keyword') as string
+export default function SearchNoticeList({
+  keyword,
+  page,
+  initTotalItems,
+  initData,
+}: {
+  keyword: string
+  page: number
+  initTotalItems: number
+  initData: AllNoticesData[]
+}) {
+  // const searchParams = useSearchParams()
+  // const keyword = searchParams.get('keyword') as string
 
   return (
     <div>
-      <NoticeList title={`${keyword}에 대한 공고목록`} keyword={keyword} />
+      <NoticeList
+        title={`${keyword}에 대한 공고목록`}
+        keyword={keyword}
+        page={page}
+        initTotalItems={initTotalItems}
+        initData={initData}
+      />
     </div>
   )
 }
