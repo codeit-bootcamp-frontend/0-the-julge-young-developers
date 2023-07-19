96efbc6d471fbd0621445f9525824531c44347e9
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-19 20:19:00 +0900

    :sparkles: Root 페이지 및 /search 페이지 구현

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,11 @@
+import CustomNoticeList from '@/libs/custom-notice-list/feature/custom-notice-list'
+import NoticeList from '@/libs/notice-list/feature/notice-list'
+
 export default function Home() {
-  return <main>Home</main>
+  return (
+    <main>
+      <CustomNoticeList />
+      <NoticeList />
+    </main>
+  )
 }
