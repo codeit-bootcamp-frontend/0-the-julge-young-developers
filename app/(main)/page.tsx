acf0900e9a89ea116bc0692ff6e76cea41098bca
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-07-19 22:53:26 +0900

    :sparkles: Page props 내려주도록 수정

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,46 @@
 import CustomNoticeList from '@/libs/custom-notice-list/feature/custom-notice-list'
 import NoticeList from '@/libs/notice-list/feature/notice-list'
+import { getNotices } from '@/libs/shared/api/data-access/request/noticeRequest'
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string }
+}) {
+  let itemArray
+  let initTotalItems
+
+  const page =
+    Number.isNaN(Number(searchParams.page)) || Number(searchParams.page) < 1
+      ? 1
+      : Math.floor(Number(searchParams.page))
+
+  const TABLES_ITEMS_PER_PAGE = 6
+  const offset = (page - 1) * TABLES_ITEMS_PER_PAGE
+  const limit = TABLES_ITEMS_PER_PAGE
+
+  const response = await getNotices({ limit, offset })
+  if (response instanceof Error) {
+    // 알 수 없는 에러 처리
+  } else if (typeof response === 'string') {
+    // 에러 메시지에 맞게 처리
+  } else {
+    // response 데이터 가공
+    const { items, count } = response
+    initTotalItems = count
+    itemArray = items.map((item) => item.item)
+  }
+
   return (
     <main>
       <CustomNoticeList />
-      <NoticeList />
+      {itemArray && (
+        <NoticeList
+          page={page}
+          initTotalItems={initTotalItems as number}
+          initData={itemArray}
+        />
+      )}
     </main>
   )
 }
