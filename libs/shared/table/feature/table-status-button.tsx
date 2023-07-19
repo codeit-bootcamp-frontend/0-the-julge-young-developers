24ffc572cb309aadb222bb2beb4138ea5c8154db
drizzle96 <dlxkffldk37@naver.com>
2023-07-19 14:03:37 +0900

    :sparkles: 사장 가게 상세 페이지의 도메인, 클라이언트 컴포넌트에 로딩 적용

--- libs/shared/table/feature/table-status-button.tsx
+++ libs/shared/table/feature/table-status-button.tsx
@@ -10,11 +10,12 @@ import {
   ActiveOutlineConfirmBtn,
 } from '@/libs/shared/click-btns/feature/click-btns'
 import { ActionDialog } from '@/libs/shared/dialog/feature/dialog'
+import CommonClientLoader from '@/libs/shared/loading/feature/client-loader'
 import { useMediaQuery } from '@/libs/shared/shared/util/useMediaQuery'
 import useOutsideClick from '@/libs/shared/shared/util/useOutsideClick'
 import { TableStatusButtonProps } from '@/libs/shared/table/type-table'
-
-import UiTableBodyStatusChip from '../ui/ui-table-composition/ui-table-body-status-chip'
+import UiTableBodyStatusChip from '@/libs/shared/table/ui/ui-table-composition/ui-table-body-status-chip'
+import ToastContainer from '@/libs/shared/toast/feature/toast-container'
 
 export default function TableStatusButton({
   shopId,
@@ -25,36 +26,45 @@ export default function TableStatusButton({
   const isMobile = useMediaQuery('(max-width: 768px)')
   const [isMobileSize, setIsMobileSize] = useState<boolean>(false)
 
-  const [isRejectDone, setIsRejectDone] = useState(false)
-
   const [shownAcceptDialog, setShownAcceptDialog] = useState(false)
   const [shownRejectDialog, setShownRejectDialog] = useState(false)
-
   const rejectDialogRef = useRef<HTMLDivElement | null>(null)
   const acceptDialogRef = useRef<HTMLDivElement | null>(null)
   useOutsideClick(rejectDialogRef, () => setShownRejectDialog(false))
   useOutsideClick(acceptDialogRef, () => setShownAcceptDialog(false))
 
+  const [openClientLoader, setOpenClientLoader] = useState(false)
+  const [shownToast, setShownToast] = useState(false)
+  const [isRejectDone, setIsRejectDone] = useState(false)
+
   const handleClickAcceptApplication = async () => {
-    setShownAcceptDialog(false)
+    setOpenClientLoader(true)
     await updateNoticeApplicationResult(
       shopId,
       noticeId,
       applicationId,
       'accepted',
     )
+    setOpenClientLoader(false)
+
+    setShownToast(true)
+    setShownAcceptDialog(false)
     router.push('/my-shop')
   }
 
   const handleClickRejectApplication = async () => {
-    setShownRejectDialog(false)
-    setIsRejectDone(true)
+    setOpenClientLoader(true)
     await updateNoticeApplicationResult(
       shopId,
       noticeId,
       applicationId,
       'rejected',
     )
+    setOpenClientLoader(false)
+
+    setShownToast(true)
+    setShownRejectDialog(false)
+    setIsRejectDone(true)
   }
 
   useEffect(() => {
@@ -97,6 +107,12 @@ export default function TableStatusButton({
             onAccept={handleClickAcceptApplication}
           />
         </div>
+      )}
+      {openClientLoader && <CommonClientLoader />}
+      {shownToast && (
+        <ToastContainer onShow={() => setShownToast(false)}>
+          {shownAcceptDialog ? '승인했어요' : '거절했어요'}
+        </ToastContainer>
       )}
     </>
   )
