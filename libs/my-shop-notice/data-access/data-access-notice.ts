879d8b6180838643c9c042bce958ac5229107f55
drizzle96 <dlxkffldk37@naver.com>
2023-07-20 10:53:29 +0900

    :recycle: 사장 가게 공고 상세 서버 데이터 get 상황 에러 처리

--- libs/my-shop-notice/data-access/data-access-notice.ts
+++ libs/my-shop-notice/data-access/data-access-notice.ts
@@ -2,10 +2,7 @@ import { getNoticeApplicationList } from '@/libs/shared/api/data-access/request/
 import { getShopNotice } from '@/libs/shared/api/data-access/request/noticeRequest'
 import { NoticeDetail } from '@/libs/shared/notice-card/type-notice-card'
 import { utilFormatDuration } from '@/libs/shared/shared/util/utilFormatDuration'
-import { MOCK_APPLICANT_LIST_DATA } from '@/libs/shared/table/data-access/data-access-mock'
 import { ApplicantList } from '@/libs/shared/table/type-table'
-
-import { MOCK_NOTICE_DETAIL_DATA } from './data-access-mock'
 
 export const getNoticeDetailData = async (
   shopId: string,
@@ -13,54 +10,51 @@ export const getNoticeDetailData = async (
 ): Promise<NoticeDetail> => {
   const res = await getShopNotice(shopId, noticeId)
   if (res instanceof Error) {
-    // 알 수 없는 에러 처리
-    return MOCK_NOTICE_DETAIL_DATA
-  }
-  if (typeof res === 'string') {
-    // 에러 메시지에 맞게 처리
-    return MOCK_NOTICE_DETAIL_DATA
-  }
-  // response 데이터 가공
-  const notice = res.item
-  const {
-    id,
-    hourlyPay,
-    startsAt,
-    workhour,
-    description: noticeDescription,
-    closed,
-    shop,
-  } = notice
+    throw new Error()
+  } else if (typeof res === 'string') {
+    throw new Error(res)
+  } else {
+    const notice = res.item
+    const {
+      id,
+      hourlyPay,
+      startsAt,
+      workhour,
+      description: noticeDescription,
+      closed,
+      shop,
+    } = notice
 
-  const {
-    name,
-    category,
-    address1: address,
-    description: shopDescription,
-    imageUrl,
-    originalHourlyPay,
-  } = shop.item
+    const {
+      name,
+      category,
+      address1: address,
+      description: shopDescription,
+      imageUrl,
+      originalHourlyPay,
+    } = shop.item
 
-  const noticeDetail = {
-    id,
-    name,
-    category,
-    duration: utilFormatDuration(startsAt, workhour),
-    startsAt,
-    workhour,
-    address,
-    shopDescription,
-    noticeDescription,
-    hourlyPay,
-    originalHourlyPay,
-    imageUrl,
-    closed,
+    const noticeDetail = {
+      id,
+      name,
+      category,
+      duration: utilFormatDuration(startsAt, workhour),
+      startsAt,
+      workhour,
+      address,
+      shopDescription,
+      noticeDescription,
+      hourlyPay,
+      originalHourlyPay,
+      imageUrl,
+      closed,
+    }
+
+    return noticeDetail
   }
-
-  return noticeDetail
 }
 
-const TABLES_ITEMS_PER_PAGE = Number(process.env.TABLE_ITEMS_PER_PAGE)
+const TABLES_ITEMS_PER_PAGE = 5
 export const getNoticeApplicantsData = async (
   shopId: string,
   noticeId: string,
@@ -70,25 +64,20 @@ export const getNoticeApplicantsData = async (
   const limit = TABLES_ITEMS_PER_PAGE
   const res = await getNoticeApplicationList(shopId, noticeId, offset, limit)
   if (res instanceof Error) {
-    // 알 수 없는 에러 처리
-    // MOCK 데이터로 임시 에러 처리
-    return [MOCK_APPLICANT_LIST_DATA, MOCK_APPLICANT_LIST_DATA.length]
-  }
-  if (typeof res === 'string') {
-    // 에러 메시지에 맞게 처리
-    // MOCK 데이터로 임시 에러 처리
-    return [MOCK_APPLICANT_LIST_DATA, MOCK_APPLICANT_LIST_DATA.length]
-  }
-  // response 데이터 가공
-  const applications = res.items
+    throw new Error()
+  } else if (typeof res === 'string') {
+    throw new Error(res)
+  } else {
+    const applications = res.items
 
-  const noticeApplicants = applications.map((application) => ({
-    id: application.item.id,
-    status: application.item.status,
-    name: application.item.user?.item.name,
-    description: application.item.user?.item.bio,
-    phone: application.item.user?.item.phone,
-  }))
+    const noticeApplicants = applications.map((application) => ({
+      id: application.item.id,
+      status: application.item.status,
+      name: application.item.user?.item.name,
+      description: application.item.user?.item.bio,
+      phone: application.item.user?.item.phone,
+    }))
 
-  return [noticeApplicants, res.count]
+    return [noticeApplicants, res.count]
+  }
 }
