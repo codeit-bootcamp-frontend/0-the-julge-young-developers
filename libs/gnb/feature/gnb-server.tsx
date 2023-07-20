8f5eb63a0fe2012099b082facd6b4cebb9a13879
wooleejaan <wooleejaan@gmail.com>
2023-07-20 10:58:16 +0900

    :sparkles: Gnb 서버 컴포넌트로 Wrapping

---
+++ libs/gnb/feature/gnb-server.tsx
@@ -0,0 +1,39 @@
+import { cookies } from 'next/headers'
+
+import { getUserInfo } from '@/libs/shared/api/data-access/request/userRequest'
+
+import Gnb from './gnb'
+
+export const revalidate = 3600
+export default async function GnbServer() {
+  const cookieInstance = cookies()
+  const userId = cookieInstance.get('uid')?.value
+
+  let userType
+  let sid
+
+  if (!userId) {
+    userType = 'guest'
+  } else {
+    const userInfo = await getUserInfo(userId)
+
+    if (userInfo instanceof Error) {
+      // 알 수 없는 에러 처리
+    } else if (typeof userInfo === 'string') {
+      // 에러 메시지에 맞게 처리
+    } else {
+      // res 데이터 가공
+      userType = userInfo.item.type
+      if (userType === 'employer' && userInfo.item.shop) {
+        sid = userInfo.item.shop.item.id
+      }
+    }
+  }
+
+  return (
+    <Gnb
+      userType={userType as 'guest' | 'employee' | 'employer' | undefined}
+      sid={sid}
+    />
+  )
+}
--- libs/gnb/feature/gnb.tsx
+++ libs/gnb/feature/gnb.tsx
@@ -2,56 +2,34 @@
 
 import { useEffect, useState } from 'react'
 
-import { getCookie, setCookie } from 'cookies-next'
+import { setCookie } from 'cookies-next'
 
 import { useRouter } from 'next/navigation'
 
 import Searchbar from '@/libs/gnb/feature/searchbar'
 import UiGnb from '@/libs/gnb/ui/ui-gnb/ui-gnb'
-import { getUserInfo } from '@/libs/shared/api/data-access/request/userRequest'
 
-export default function Gnb() {
+interface GnbProps {
+  userType?: 'guest' | 'employee' | 'employer'
+  sid?: string
+}
+
+export default function Gnb({ userType, sid }: GnbProps) {
   const [hasNotification, setHasNotification] = useState(false)
-  const [userType, setUserType] = useState<
-    'employee' | 'employer' | 'guest' | null
-  >(null)
+
   const router = useRouter()
-  const userId = getCookie('uid') as string
 
   const handleCheckNotification = () => {
     setHasNotification(true)
   }
 
-  const getUserType = async (uid: string) => {
-    const userInfo = await getUserInfo(uid)
-    if (userInfo instanceof Error) {
-      // 알 수 없는 에러 처리
-    } else if (typeof userInfo === 'string') {
-      // 에러 메시지에 맞게 처리
-    } else {
-      // res 데이터 가공
-      setUserType(userInfo.item.type)
-      if (userInfo.item.type === 'employer' && userInfo.item.shop) {
-        setCookie('sid', userInfo.item.shop.item.id)
-      }
-    }
-  }
-
-  useEffect(() => {
-    const fetchUserType = async () => {
-      if (!userId) {
-        setUserType('guest')
-      } else {
-        await getUserType(userId)
-      }
-    }
-
-    fetchUserType()
-  }, [userId])
-
   const handleClickMovePage = (pathname?: string) => {
     router.push(`/${pathname}`)
   }
+
+  useEffect(() => {
+    setCookie('sid', sid)
+  }, [sid])
 
   return (
     <UiGnb
