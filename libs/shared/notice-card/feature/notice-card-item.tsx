c4feef35571348bd1a718311845d385214b6e29f
wooleejaan <wooleejaan@gmail.com>
2023-07-20 11:50:01 +0900

    :sparkles: 공고 목록 없을 때 보여줄 빈 문구

--- libs/shared/notice-card/feature/notice-card-item.tsx
+++ libs/shared/notice-card/feature/notice-card-item.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import classNames from 'classnames/bind'
 import { getCookie } from 'cookies-next'
 
 import { useRouter } from 'next/navigation'
+
+import styles from '@/libs/shared/notice-card/feature/notice-card-item.module.scss'
 
 import { AllNoticesData } from '../../api/types/type-notice'
 import UiNoticeCardItem from '../ui/ui-notice-card-item/ui-notice-card-item'
@@ -10,6 +13,8 @@ import UiNoticeCardList from '../ui/ui-notice-card-list/ui-notice-card-list'
 import { utilCalcChangeRate } from '../util/util-calc-change-rate'
 import { utilCalcPayDiff } from '../util/util-calc-pay-diff'
 import { utilFormatDuration } from '../util/util-format-duration'
+
+const cx = classNames.bind(styles)
 
 export default function NoticeCardItem({
   isHome,
@@ -54,12 +59,17 @@ export default function NoticeCardItem({
       handleClickToDetail(item.closed, item.shop.item.id, item.id),
   }))
 
+  console.log(itemDatas)
+
   return (
     <UiNoticeCardList
       isHome={isHome}
       title={title}
       filterElement={filterElement}
     >
+      {itemDatas.length === 0 && (
+        <div className={cx('blankWrapper')}>공고 목록이 존재하지 않아요.</div>
+      )}
       {itemDatas.map((notice) => (
         <UiNoticeCardItem
           key={notice.id}
